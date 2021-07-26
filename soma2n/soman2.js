537bb4f0b5af89183b28619b0fe5b290cdc4a6a6
learnandmake <gabrod7k@gmail.com>
2021-07-26 19:40:00 -0300

    a

---
+++ soma2n/soman2.js
@@ -0,0 +1,11 @@
+function calc2n ()
+{
+    console.log("Vou calcular dois numeros");
+    let n1 =prompt("digite o primeiro numero");
+    let n2 =prompt("digite o segundo numero");
+
+    var total=parseInt(n1)+parseInt(n2);
+    console.log("A soma total é:"+" "+total);
+}
+
+calc2n();
