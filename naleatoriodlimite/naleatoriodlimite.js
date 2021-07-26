cbb26b00dff347dcf5d39fafe7a05b536405d015
learnandmake <gabrod7k@gmail.com>
2021-07-26 20:46:42 -0300

    a

---
+++ naleatoriodlimite/naleatoriodlimite.js
@@ -0,0 +1,18 @@
+function numrandom()
+{
+    console.log("vou contar 10 numeros aleatorios");
+    let contagem1 =prompt("digite qualquer limite");
+    var numero= 0;
+    var contagem=0;
+    while(contagem<=contagem1)
+{
+numero=Math.random();
+console.log(numero)
+contagem++;
+if (contagem>contagem1)
+{
+    console.log("a contagem terminou");
+}
+}
+
+}
